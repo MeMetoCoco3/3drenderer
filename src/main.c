daf5354720d909896b869b7973693ea1eddc7847
MeMetoCoco3 <torresmartinezvidal@gmail.com>
2025-04-29 20:08:16 +0200

    Rendering color buffer and texture buffer.

--- src/main.c
+++ src/main.c
@@ -4,11 +4,19 @@
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL_video.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
+
+bool is_running;
 
 SDL_Window *window;
 SDL_Renderer *renderer;
-bool is_running;
+
+uint32_t *color_buffer;
+SDL_Texture *color_buffer_texture;
+
+int window_height = 600;
+int window_width = 800;
 
 bool init_window(void) {
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -16,9 +24,16 @@ bool init_window(void) {
     fprintf(stderr, "Error initializing SDL.\n");
     return false;
   }
+
+  // Query SDL to know size of display.
+  SDL_DisplayMode display_mode;
+  SDL_GetCurrentDisplayMode(0, &display_mode);
+  window_height = display_mode.h;
+  window_width = display_mode.w;
+
   window =
       SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                       800, 600, SDL_WINDOW_BORDERLESS);
+                       window_width, window_height, SDL_WINDOW_BORDERLESS);
   if (!window) {
     fprintf(stderr, "Error initializing Window.\n");
     return false;
@@ -30,10 +45,28 @@ bool init_window(void) {
     fprintf(stderr, "Error initializing Renderer.\n");
     return false;
   }
+  // Set Fullscreen.
+  SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
   return true;
 }
 
-void setup(void) {}
+void setup(void) {
+  color_buffer =
+      (uint32_t *)malloc(sizeof(uint32_t) * window_width * window_height);
+  color_buffer_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
+                                           SDL_TEXTUREACCESS_STREAMING,
+                                           window_width, window_height);
+}
+/* FROM EXERCICE DRAWING A BACKGROUND GRID*/
+void draw_grid(void) {
+  for (int y = 0; y < window_height; y++) {
+    for (int x = 0; x < window_width; x++)
+      if (y % 10 == 0 || x % 10 == 0) {
+        color_buffer[y * window_width + x] = 0xFF000000;
+      }
+  }
+}
+
 void get_input(void) {
   SDL_Event event;
   SDL_PollEvent(&event);
@@ -49,13 +82,45 @@ void get_input(void) {
   }
 }
 void update(void) {}
+
+void render_color_buffer(void) {
+  // Updates texture with the values from color buffer.
+  SDL_UpdateTexture(color_buffer_texture, NULL, color_buffer,
+                    (int)sizeof(uint32_t) * window_width);
+  // Renders texture on device.
+  SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
+}
+void clear_color_buffer(uint32_t color) {
+  for (int y = 0; y < window_height; y++) {
+    for (int x = 0; x < window_width; x++)
+      color_buffer[y * window_width + x] = color;
+  }
+}
+
 void render(void) {
   // Sets a color.
-  SDL_SetRenderDrawColor(renderer, 200, 0, 255, 255);
+  SDL_SetRenderDrawColor(renderer, 200, 0, 0, 255);
   // Draws that color on backbuffer.
   SDL_RenderClear(renderer);
+  render_color_buffer();
+  clear_color_buffer(0xFFFFFF00);
+  draw_grid();
   // Sends backbuffer to Window.
   SDL_RenderPresent(renderer);
+}
+
+void destroy_window(void) {
+  free(color_buffer);
+  printf("Color buffer freed.\n");
+
+  SDL_DestroyRenderer(renderer);
+  printf("Renderer freed.\n");
+
+  SDL_DestroyWindow(window);
+  printf("Window freed.\n");
+
+  SDL_Quit();
+  printf("SDL QUIT.\n");
 }
 
 int main(void) {
@@ -66,6 +131,6 @@ int main(void) {
     update();
     render();
   }
-
+  destroy_window();
   return 0;
 }
